0141443e8333fc2688a4ce61f31eb556103aeaf0
rajeshpatra <p7.rajesh@gmail.com>
2017-01-23 00:23:31 +0530

    added create method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,19 @@ class ProductsController < ApplicationController
 		@product = Product.new
 	end
 
+	def create
+		@product = Product.new(product_params)
+
+		respond_to do |format|
+			if @product.save
+				format.html { redirect_to @product, notice: 'Product was successfully created.' }
+			else
+				format.html { render :new }
+			end
+		end
+	end
+
+	private
 	def product_params
 		params.require(:product).permit(:title, :price, :description, :image_url)
 	end
