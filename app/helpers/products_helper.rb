b185cd33d3d0b61c4c17006fe4752bc6f8056c39
rajeshpatra <p7.rajesh@gmail.com>
2017-01-23 16:21:32 +0530

    added code to print stock

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,4 +4,14 @@ module ProductsHelper
 		# format("$%.2f", price)
 		number_to_currency price
 	end
+
+	def print_stock(stock)
+		if stock > 0
+			# <span class="in_stock">In Stock(12)</span>
+			"In stock"
+		else
+			# <span class="out_stock">Out of Stock</span>"
+			"Out of stock"
+		end
+	end
 end
