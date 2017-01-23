6feeeef1e17e1a861ea3ad95af2595ab6a5dcef8
rajeshpatra <p7.rajesh@gmail.com>
2017-01-23 14:45:00 +0530

    added new column to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121191251) do
+ActiveRecord::Schema.define(version: 20170123090942) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
