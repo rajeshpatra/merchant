9b6b09cce1ad6040421bc1dc86cd549866ecbfad
rajeshpatra <p7.rajesh@gmail.com>
2017-01-24 14:38:52 +0530

    added association and validation

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,4 @@
 class OrderItem < ActiveRecord::Base
+	belongs_to :order, validate: true
+	belongs_to :product, validate: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
 	validates_numericality_of :price
 
 	validates_numericality_of :stock, only_integer: true
+
+	has_many :order_items
 end
