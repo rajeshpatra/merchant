b5974cdd1bf3bedfe9b72945d00244c3d8e049cc
rajeshpatra <p7.rajesh@gmail.com>
2017-01-25 10:40:10 +0530

    added a method subtotal for a quantity's total amount

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,9 @@
 class OrderItem < ActiveRecord::Base
 	belongs_to :order, validate: true
 	belongs_to :product, validate: true
+
+	def subtotal
+		# @order.order_items.each do |item|
+		# subtotal code for every item goes here
+	end
 end
