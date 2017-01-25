56b12cd50a31a4ea6c9e18a727c1371cba807f57
rajeshpatra <p7.rajesh@gmail.com>
2017-01-26 01:22:29 +0530

    added redirection path after updating item  quantity

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -27,9 +27,9 @@ class OrderItemsController < ApplicationController
   # POST /order_items
   # POST /order_items.json
   def create
-    # @order_item = @order.order_items.new(quantity: 1, product_id: params[:product_id])
+    @order_item = @order.order_items.new(quantity: 1, product_id: params[:product_id])
 
-    @order_item = @order.order_items.find_or_initialize_by_product_id(params[:product_id])
+    # @order_item = @order.order_items.find_or_initialize_by_product_id(params[:product_id])
 
     respond_to do |format|
       if @order_item.save
@@ -47,7 +47,7 @@ class OrderItemsController < ApplicationController
   def update
     respond_to do |format|
       if @order_item.update(order_item_params)
-        format.html { redirect_to @order_item, notice: 'Order item was successfully updated.' }
+        format.html { redirect_to order_path, notice: 'Order item was successfully updated.' }
         format.json { render :show, status: :ok, location: @order_item }
       else
         format.html { render :edit }
