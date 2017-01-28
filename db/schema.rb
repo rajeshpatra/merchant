69db2f34a155352510d2ec62d8366122a1db4703
rajeshpatra <p7.rajesh@gmail.com>
2017-01-28 20:04:32 +0530

    added column address_id to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128133227) do
+ActiveRecord::Schema.define(version: 20170128143339) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "line1"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170128133227) do
     t.string   "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "address_id"
   end
 
   create_table "products", force: :cascade do |t|
