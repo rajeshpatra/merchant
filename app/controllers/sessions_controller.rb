2dc9896491f0da241a90f9100bd63c7a3f0a298a
rajeshpatra <p7.rajesh@gmail.com>
2017-01-28 14:10:38 +0530

    added redirection to products after successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,7 @@ class SessionsController < ApplicationController
 
 	def create
 		@user = User.find_or_create_by_auth(request.env["omniauth.auth"])
+		session[:user_id] = @user.id
+		redirect_to products_path, notice: "Logged in as #{@user.name}"
 	end
 end
