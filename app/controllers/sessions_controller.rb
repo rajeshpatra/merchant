d7a0526d0d4df355558ec686e87cd73dba5403d4
rajeshpatra <p7.rajesh@gmail.com>
2017-01-28 13:44:05 +0530

    added create method so user will handle data from twitter hash data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
+	# def create
+	# 	render text: request.env["omniauth.auth"]
+	# end
+
 	def create
-		render text: request.env["omniauth.auth"]
+		@user = User.find_or_create_by_auth(request.env["omniauth.auth"])
 	end
 end
